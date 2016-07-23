1118455c6f5e4cb9da225c16e4be52c9cee0d830
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2016-07-23 19:15:47 -0300

    add filter by city

--- mirage/config.js
+++ mirage/config.js
@@ -1,40 +1,47 @@
 export default function() {
-  this.get('/rentals', function() { 
-    return {
-      data: [{
-        type: 'rentals',
-        id: 1,
-        attributes: {
-          title: 'Grand Old Mansion',
-          owner: 'San Francisco',
-          city: 'San Francisco',
-          type: 'Estate',
-          bedrooms: 15,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
-        }
-      }, {
-        type: 'rentals',   
-        id: 2,
-        attributes: {
-          title: 'Urben Living',
-          onwer: 'Mike TV',
-          city: 'Seattle',
-          type: 'Condo',
-          bedrooms: 1,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
-       }       
-      }, {
-        type: 'rentals',
-        id: 3,
-        attributes: {
-          title: 'Downtown Charm',
-          owner: 'Violet Beauregarde',
-          city: 'Portland',
-          type: 'Apartment',
-          bedrooms: 3,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
-        }
-      }]
-    };
+  this.get('/rentals', function(db, request) { 
+    let rentals = [{
+      type: 'rentals',
+      id: 1,
+      attributes: {
+        title: 'grand old mansion',
+        owner: 'san francisco',
+        city: 'san francisco',
+        type: 'estate',
+        bedrooms: 15,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+      }
+    }, {
+      type: 'rentals',   
+      id: 2,
+      attributes: {
+        title: 'urben living',
+        onwer: 'mike tv',
+        city: 'seattle',
+        type: 'condo',
+        bedrooms: 1,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+     }       
+    }, {
+      type: 'rentals',
+      id: 3,
+      attributes: {
+        title: 'downtown charm',
+        owner: 'violet beauregarde',
+        city: 'portland',
+        type: 'apartment',
+        bedrooms: 3,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+      }
+    }];
+    
+    if (request.queryParams.city !== undefined) {
+      let filteredRentals = rentals.filter(function(i) {
+        return i.attributes.city.toLowerCase().indexOf(request.queryParams.city.toLowerCase()) !== -1;
+      });
+      return { data: filteredRentals };
+    } else {
+      return { data: rentals };
+    }
   });
 }
